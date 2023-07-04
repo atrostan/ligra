0d244c3f7435ab2acb9d51ea77563670b050436d
atrostan <atrostan@cs.ubc.ca>
2023-07-04 14:00:31 -0700

    use opencilk compiler

--- apps/CF.C
+++ apps/CF.C
@@ -20,57 +20,58 @@
 
 #define WEIGHTED 1
 #include "ligra.h"
+#include <vector>
 //uncomment the following line to compute the sum of squared errors per iteration
-//#define COMPUTE_ERROR 1
+// #define COMPUTE_ERROR 1
 //uncomment the following line to print out the sum of values in latent vector
 //#define DEBUG 1
 
 #ifdef COMPUTE_ERROR
-double* squaredErrors;
+double *squaredErrors;
 #endif
 
-template <class vertex>
+template<class vertex>
 struct CF_Edge_F {
-  double* latent_curr, *error;
-  vertex* V;
+  double *latent_curr, *error;
+  vertex *V;
   int K;
-  CF_Edge_F(vertex* _V, double* _latent_curr, double* _error, int _K) : 
-    V(_V), latent_curr(_latent_curr), error(_error), K(_K) {}
-  inline bool update(uintE s, uintE d, intE edgeLen){ //updates latent vector based on neighbors' data
+  CF_Edge_F(vertex *_V, double *_latent_curr, double *_error, int _K) : V(_V), latent_curr(_latent_curr), error(_error), K(_K) {}
+  inline bool update(uintE s, uintE d, intE edgeLen) {//updates latent vector based on neighbors' data
+    // std::cout << s << " " << d << " " << edgeLen << "\n";
     double estimate = 0;
-    long current_offset = K*d, ngh_offset = K*s;
-    double *cur_latent =  &latent_curr[current_offset], *ngh_latent = &latent_curr[ngh_offset];
-    for(int i = 0; i < K; i++){
-      estimate +=  cur_latent[i]*ngh_latent[i];
+    long current_offset = K * d, ngh_offset = K * s;
+    double *cur_latent = &latent_curr[current_offset], *ngh_latent = &latent_curr[ngh_offset];
+    for (int i = 0; i < K; i++) {
+      estimate += cur_latent[i] * ngh_latent[i];
     }
     double err = edgeLen - estimate;
 
 #ifdef COMPUTE_ERROR
-    squaredErrors[d] += err*err;
+    squaredErrors[d] += err * err;
 #endif
 
-    double* cur_error = &error[current_offset];
-    for (int i = 0; i < K; i++){
-      cur_error[i] += ngh_latent[i]*err;
+    double *cur_error = &error[current_offset];
+    for (int i = 0; i < K; i++) {
+      cur_error[i] += ngh_latent[i] * err;
     }
     return 1;
   }
-  inline bool updateAtomic (uintE s, uintE d, intE edgeLen) {
+  inline bool updateAtomic(uintE s, uintE d, intE edgeLen) {
     //not needed as we will always do pull based
-    return update(s,d,edgeLen);
+    return update(s, d, edgeLen);
   }
-  inline bool cond (intT d) { return cond_true(d); }};
+  inline bool cond(intT d) { return cond_true(d); }
+};
 
 struct CF_Vertex_F {
   double step, lambda;
   double *latent_curr, *error;
   int K;
-  CF_Vertex_F(double _step, double _lambda, double* _latent_curr, double* _error, int _K) :
-    step(_step), lambda(_lambda), latent_curr(_latent_curr), error(_error), K(_K) {}
-  inline bool operator () (uintE i) {
-    for (int j = 0; j < K; j++){
-      latent_curr[K*i + j] += step*(-lambda*latent_curr[K*i + j] + error[K*i + j]);
-      error[K*i+j] = 0.0;
+  CF_Vertex_F(double _step, double _lambda, double *_latent_curr, double *_error, int _K) : step(_step), lambda(_lambda), latent_curr(_latent_curr), error(_error), K(_K) {}
+  inline bool operator()(uintE i) {
+    for (int j = 0; j < K; j++) {
+      latent_curr[K * i + j] += step * (-lambda * latent_curr[K * i + j] + error[K * i + j]);
+      error[K * i + j] = 0.0;
     }
 #ifdef COMPUTE_ERROR
     squaredErrors[i] = 0;
@@ -79,64 +80,68 @@ struct CF_Vertex_F {
   }
 };
 
-template <class vertex>
-void Compute(graph<vertex>& GA, commandLine P) {
-  int K = P.getOptionIntValue("-K",20); //dimensions of the latent vector  
-  int numIter = P.getOptionIntValue("-numiter",5); //number of iterations
-  double step = P.getOptionDoubleValue("-step",0.00000035); //step size
-  double lambda = P.getOptionDoubleValue("-lambda",0.001); //lambda
-  bool randInit = P.getOption("-randInit"); //pass flag for random initialization of latent vector
+template<class vertex>
+void Compute(graph<vertex> &GA, commandLine P) {
+  int K = P.getOptionIntValue("-K", 20);                    //dimensions of the latent vector
+  int numIter = P.getOptionIntValue("-numiter", 5);         //number of iterations
+  double step = P.getOptionDoubleValue("-step", 0.00000035);//step size
+  double lambda = P.getOptionDoubleValue("-lambda", 0.001); //lambda
+  bool randInit = P.getOption("-randInit");                 //pass flag for random initialization of latent vector
   //initialize latent vectors and errors
   const intE n = GA.n;
-  double* latent_curr = newA(double, K*n);
-  double* error = newA(double, K*n);
+
+  double *latent_curr = newA(double, K *n);
+  double *error = newA(double, K *n);
 #ifdef COMPUTE_ERROR
-  squaredErrors = newA(double,n);
+  squaredErrors = newA(double, n);
 #endif
-  if(randInit) { 
+  if (randInit) {
     srand(0);
     long seed = rand();
-    parallel_for(uintE i = 0; i < n; i++){
+    parallel_for(uintE i = 0; i < n; i++) {
 #ifdef COMPUTE_ERROR
       squaredErrors[n] = 0;
 #endif
-      for (int j = 0; j < K; j++){
-	latent_curr[i*K+j] = ((double)(seed+hashInt((uintE)i*K+j))/(double)UINT_E_MAX);
-	error[i*K+j] = 0.0;
+      for (int j = 0; j < K; j++) {
+        latent_curr[i * K + j] = ((double) (seed + hashInt((uintE) i * K + j)) / (double) UINT_E_MAX);
+        error[i * K + j] = 0.0;
       }
     }
   } else {
-    parallel_for(uintE i = 0; i < n; i++){
+    parallel_for(uintE i = 0; i < n; i++) {
 #ifdef COMPUTE_ERROR
       squaredErrors[n] = 0;
 #endif
-      for (int j = 0; j < K; j++){
-	latent_curr[i*K+j] = 0.5; //default initial value of 0.5
-	error[i*K+j] = 0.0;
+      for (int j = 0; j < K; j++) {
+        latent_curr[i * K + j] = 0.5;//default initial value of 0.5
+        error[i * K + j] = 0.0;
       }
     }
   }
 
-  bool* frontier = newA(bool,n);
-  {parallel_for(long i=0;i<n;i++) frontier[i] = 1;}
-  vertexSubset Frontier(n,n,frontier);
+  bool *frontier = newA(bool, n);
+  { parallel_for(long i = 0; i < n; i++) frontier[i] = 1; }
+  vertexSubset Frontier(n, n, frontier);
 
-  for (int iter = 0; iter < numIter; iter++){
+  for (int iter = 0; iter < numIter; iter++) {
     //edgemap to accumulate error for each node
-    edgeMap(GA, Frontier, CF_Edge_F<vertex>(GA.V,latent_curr,error,K), 0, no_output);
+    edgeMap(GA, Frontier, CF_Edge_F<vertex>(GA.V, latent_curr, error, K), 0, no_output);
 
 #ifdef COMPUTE_ERROR
-    cout << "sum of squared error: " << sequence::plusReduce(squaredErrors,n)/2 << " for iter: " << iter << endl;
+    cout << "sum of squared error: " << sequence::plusReduce(squaredErrors, n) / 2 << " for iter: " << iter << endl;
 #endif
 
     //vertexmap to update the latent vectors
-    vertexMap(Frontier,CF_Vertex_F(step,lambda,latent_curr,error,K));
+    vertexMap(Frontier, CF_Vertex_F(step, lambda, latent_curr, error, K));
 
 #ifdef DEBUG
-    cout << "latent sum: " << sequence::plusReduce(latent_curr,K*n) << endl;
+    cout << "latent sum: " << sequence::plusReduce(latent_curr, K * n) << endl;
 #endif
   }
-  Frontier.del(); free(latent_curr); free(error);
+
+  Frontier.del();
+  free(latent_curr);
+  free(error);
 #ifdef COMPUTE_ERROR
   free(squaredErrors);
 #endif
